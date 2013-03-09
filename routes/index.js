75a4e4bec61fe4382801f4945317408f67eed401
Rohit Ghatol <rohit.ghatol@synerzip.com>
2013-03-09 12:57:12 +0530

    Working Responsive Web site created in Twitter BootStrap

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+exports.about = function(req, res){
+    res.render('about', { title: 'Express' });
+};
+
+exports.contact = function(req, res){
+    res.render('contact', { title: 'Express' });
+};
+
+exports.signup = function(req, res){
+    res.render('signup', { title: 'Express' });
+};
+
+exports.login = function(req, res){
+    res.render('login', { title: 'Express' });
+};
